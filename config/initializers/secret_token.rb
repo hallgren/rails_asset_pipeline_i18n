41175ce071818848e1c3067c2b46a33b28c69831
Morgan Hallgren <morgan.hallgren@upptec.se>
2013-05-31 22:17:17 +0200

    setup asset_pipeline_i18n

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+I18nTest::Application.config.secret_token = '2365541dd9bdf1da11fa2fe8294df7f426733fb4fa91ac3af5001df2bb756b48fcf4f9e9f54c9fc05a03f0254d5b6d5da47baac74ce50df198d78b655358887f'
